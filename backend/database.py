3a8be8c30eaa4d47e2dec249310ebc4eb04a4da5
DarshanBarma <darshanbarma2610@gmail.com>
2025-07-27 03:27:00 +0530

    Updated database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-DATABASE_URI = 'mongodb+srv://darshanbarma2610:S7aD1Z53D0yKr1Bx@expensetracker.mmsahkk.mongodb.net/?retryWrites=true&w=majority&appName=ExpenseTracker'
+DATABASE_URI = os.getenv("DATABASE_URI")
+
 client = MongoClient(DATABASE_URI)
 
 db = client.expense_db
